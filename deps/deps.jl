c7ef07f8c5b21e77d856c3d910f74227eb4d8de3
brunowu <xinzhe.wu1990@gmail.com>
2021-09-21 13:51:26 +0200

    update des.jl

--- deps/deps.jl
+++ deps/deps.jl
@@ -6,9 +6,9 @@ currentDirPath = dirname(currentFilePath)
 libpath = joinpath(currentDirPath, "usr", "lib", "liblbfgsbf.so")
 
 macro checked_lib(libname, path)
-    (Libdl.dlopen_e(path) == C_NULL) && error("Unable to load \n\n$libname ($path)
+    #(Libdl.dlopen_e(path) == C_NULL) && error("Unable to load \n\n$libname ($path)
 
-    Please re-run Pkg.build(package), and restart Julia.")
+    #Please re-run Pkg.build(package), and restart Julia.")
     quote const $(esc(libname)) = $path end
 end
 @checked_lib liblbfgsbf "$libpath"
